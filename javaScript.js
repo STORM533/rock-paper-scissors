9ef1a15bc4c9dd7bf219e186957190d79b8e5662
STORM533 <sakshamkr2006@gmail.com>
2024-12-21 02:32:16 +0530

    playRound functionality done

--- javaScript.js
+++ javaScript.js
@@ -2,20 +2,55 @@
 function  getComputerChoice(){
 const minCeil = Math.ceil(0);
 const maxFloor = Math.floor(3);
-return Math.floor(Math.random()* (maxFloor - minCeil) + minCeil);
+ if( Math.floor(Math.random()* (maxFloor - minCeil) + minCeil) === 0){
+    return console.log("rock");
+ } else if (Math.floor(Math.random()* (maxFloor - minCeil) + minCeil) === 1){
+    return console.log("paper");
+ }else{
+    return console.log("scissors");
+ }
 }
 function getHumanChoice(){
-     return prompt("Enter The Value");
+     return prompt("rock ,paper , scissors");
 }
-getHumanChoice();
-    
 
 
-if (getComputerChoice() === 0) {
-    console.log("rock") ;
-}else if (getComputerChoice() === 1){
-    console.log("paper") ;
-}else { 
-    console.log("scissors") ;
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice , computerChoice){
+    getHumanChoice()== humanChoice;
+    getComputerChoice() === computerChoice;
+    if(humanChoice === "rock" && computerChoice === "rock"){
+        return console.log("TIE");
+    }else if (humanChoice=== "paper" && computerChoice === "paper"){
+        return console.log("TIE");
+    }else if (humanChoice === "scissors" && computerChoice === "scissors"){
+        return console.log("TIE");
+    }else if(humanChoice === "rock" && computerChoice === "paper"){
+        return console.log("COMPUTER WINS");
+        computerScore++;
+    }else if (humanChoice ==="rock" && computerChoice=== "scissors"){
+        return console.log("HUMAN WINS");
+        humanScore++;
+    }else if (humanChoice ==="paper" && computerChoice=== "scissors"){
+        return console.log("COMPUTER WINS");
+        computerScore++;
+    }else if (humanChoice ==="paper" && computerChoice=== "rock"){
+        return console.log("HUMAN WINS");
+        humanScore++;   
+    }else if (humanChoice ==="scissors" && computerChoice=== "rock"){
+        return console.log("COMPUTER WINS");
+        computerScore++;
+    }else if (humanChoice ==="scissors" && computerChoice=== "paper"){
+        return console.log("HUMAN WINS");
+        humanScore++;
+    }
 }
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+console.log(humanScore);
+console.log(computerScore);
 

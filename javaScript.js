156f4e09208cd131b4c28ed93eb4990ed1bea6cf
STORM533 <sakshamkr2006@gmail.com>
2024-12-21 01:08:09 +0530

    computer functionality done

--- javaScript.js
+++ javaScript.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+
+function  getComputerChoice(){
+const minCeil = Math.ceil(0);
+const maxFloor = Math.floor(3);
+return Math.floor(Math.random()* (maxFloor - minCeil) + minCeil);
+}
+if (getComputerChoice() === 0) {
+    console.log("rock") ;
+}else if (getComputerChoice() === 1){
+    console.log("paper") ;
+}else { 
+    console.log("scissors") ;
+}
+console.log(getComputerChoice());
+

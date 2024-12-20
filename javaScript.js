56eef23c6bb3d97bad943708384616c539f499a8
STORM533 <sakshamkr2006@gmail.com>
2024-12-21 01:27:47 +0530

    human functionality done

--- javaScript.js
+++ javaScript.js
@@ -4,6 +4,13 @@ const minCeil = Math.ceil(0);
 const maxFloor = Math.floor(3);
 return Math.floor(Math.random()* (maxFloor - minCeil) + minCeil);
 }
+function getHumanChoice(){
+     return prompt("Enter The Value");
+}
+getHumanChoice();
+    
+
+
 if (getComputerChoice() === 0) {
     console.log("rock") ;
 }else if (getComputerChoice() === 1){
@@ -11,5 +18,4 @@ if (getComputerChoice() === 0) {
 }else { 
     console.log("scissors") ;
 }
-console.log(getComputerChoice());
 

9b6427a21deaf2f35fd973445657009bfbf1212d
STORM533 <sakshamkr2006@gmail.com>
2024-12-22 22:56:35 +0530

    created the functionality for 5 rounds used loops to shorten the code

--- javaScript.js
+++ javaScript.js
@@ -2,27 +2,33 @@
 function  getComputerChoice(){
 const minCeil = Math.ceil(0);
 const maxFloor = Math.floor(3);
- if( Math.floor(Math.random()* (maxFloor - minCeil) + minCeil) === 0){
-    return "rock";
- } else if (Math.floor(Math.random()* (maxFloor - minCeil) + minCeil) === 1){
-    return "paper";
- }else{
-    return "scissors";
- }
+    if( Math.floor(Math.random()* (maxFloor - minCeil) + minCeil) === 0){
+        return "rock";
+    } else if (Math.floor(Math.random()* (maxFloor - minCeil) + minCeil) === 1){
+        return "paper";
+    }else{
+        return "scissors";
+    }
 }
+
+
+
+
+
 function getHumanChoice(){
-     userChoice = prompt("rock ,paper , scissors");
-     
-     return userChoice.toLowerCase();
+    userChoice = prompt("rock , paper , scissors");
+    return userChoice.toLowerCase();
 }
+    
+    
+
 
 
 let humanScore = 0;
 let computerScore = 0;
 
 function playRound(humanChoice , computerChoice){
-    humanChoice = getHumanChoice();
-    computerChoice = getComputerChoice();
+
     if((humanChoice === "rock" && computerChoice === "rock") || (humanChoice=== "paper" && computerChoice === "paper") || (humanChoice === "scissors" && computerChoice === "scissors")){
         return console.log("TIE COMPUTER CHOSE: " + `${computerChoice}` + " AND YOU CHOSE : " + `${humanChoice}`);
     }else if(humanChoice === "rock" && computerChoice === "paper"){
@@ -45,19 +51,25 @@ function playRound(humanChoice , computerChoice){
         return console.log("YOU WIN! COMPUTER CHOSE: " + `${computerChoice}` + " AND YOU CHOSE : " + `${humanChoice}`);
     }
 }
-const humanSelection = getHumanChoice();
-const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection);
 
-    console.log(humanScore);
-    console.log(computerScore);
 
 
 function playGame(){
-    for(let i = 0 ;i <= 5 ; i++){
-        playRound(humanSelection ,computerSelection);
+
+    for(let i = 1; i <= 5; i++){
+        const humanSelection = getHumanChoice();
+        const computerSelection = getComputerChoice();
+        console.log("round : " + i);
+        playRound(humanSelection , computerSelection);
+
     }
+    
 }
 playGame();
+console.log("humanScore : "  + humanScore);
+console.log("computerScore : "  + computerScore);
+
+
+
 
